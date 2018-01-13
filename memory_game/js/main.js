f7033374b2fd2ee373300cfb007299b8cf597067
Brittany A. Barden <BBarden@Brittanys-MBP.fios-router.home>
2018-01-13 14:27:48 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,14 @@
 console.log("Up and running!");
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+cardsInPlay.push(cards[cardId]);
+var flipCard = function(cardId) {
 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!")
 	} else {
 		alert("Sorry try again.")
 	};
+console.log("User flipped " + cards[cardId]);
+};
+flipCard(0);
+
